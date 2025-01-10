8fb267f39d9be55ffff893b444a4b2b00ee1a6d9
Danny <megaslashgt123@gmail.com>
2025-01-10 13:52:32 -0500

    Partner 2 Changes -> Part 2

--- src/Main.java
+++ src/Main.java
@@ -3,5 +3,9 @@ public class Main {
         System.out.println("bing bong");
         System.out.println("Test");
         System.out.println("chicken");
+
+        int firstNum = 5;
+        int secondNum = 5;
+        System.out.println("Sum: " + firstNum + secondNum);
     }
 }

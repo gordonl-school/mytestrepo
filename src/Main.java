63dc1346834febe22576309875f4557c035bac39
BT_1N3_03 <222222222@>
2025-01-10 13:57:06 -0500

    changed print

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("bing bong");
         System.out.println("Test");
-        System.out.println("chicken");
+        System.out.println("mushroom");
 
         int firstNum = 5;
         int secondNum = 5;

50aa7c8ab4ed04d101138987c474220c85b650b6
Danny <megaslashgt123@gmail.com>
2025-01-10 13:54:35 -0500

    Partner 2 Changes -> Part 3

--- src/SomeClass.java
+++ src/SomeClass.java
@@ -1,2 +1,14 @@
 public class SomeClass {
+    private int age;
+    private String name;
+
+    public SomeClass(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public String hello() {
+        return "Hello " + name + ". You are " + age + " years old.";
+    }
+
 }

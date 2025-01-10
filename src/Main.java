3096a1f3a034cdef4bdb89c510045a13ec2e546e
BT_1N3_03 <222222222@>
2025-01-10 13:54:23 -0500

    added more print

--- src/Main.java
+++ src/Main.java
@@ -3,5 +3,7 @@ public class Main {
         System.out.println("bing bong");
         System.out.println("Test");
         System.out.println("chicken");
+        System.out.println("I love the subway");
+        System.out.println("bing bong again");
     }
 }
